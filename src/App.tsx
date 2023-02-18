631488edee081ad1371d96c52f9491ebe308d3f1
Alma-GH <fgrgdgs21@gmail.com>
2023-02-18 14:19:04 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import Test from "./components/Test/Test";
+
+function App() {
+
+
+  return (
+      <div className="App">
+          <Test/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import cls from "./Test.module.scss"
+
+const Test = () => {
+
+
+
+    return (
+        <div className={cls.parent}>
+
+        </div>
+    );
+};
+
+export default Test;
